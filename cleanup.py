5dd39e663491426ff717f15b1abe471d31e5a342
Søren Reinke <srei@foss.dk>
2019-05-02 15:47:39 +0200

    Now getting filestamp of files

--- cleanup.py
+++ cleanup.py
@@ -1,30 +1,46 @@
 import os
+import datetime
 
 
 def GetFiles(path):
     result = {}
+
     for r, d, f in os.walk(path):
         for directory in d:
             path = os.path.join(r, directory)
+
             for root, directories, files in os.walk(path):
                 filesInDirectory = []
+
                 for file in files:
                     filesInDirectory.append(file)
+
             result[path] = filesInDirectory
+
     return result
 
 
-def GetSpecificFiles(files, extension):
-    result = filter(lambda k: extension in k, files)
-    return '\n'.join(map(str, result))
+def FilesToRemove(files, extension):
+    for key, value in files.items():
+        filteredFiles = [file for file in value if extension in file]
+
+        for file in filteredFiles:
+            fullPath = os.path.join(key, file)
+            print(modification_date(fullPath), file)
+
+    return ""
+
+
+def modification_date(filename):
+    t = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(t)
 
 
 def main():
     files = GetFiles("Books")
 
-    #pdfFiles = GetSpecificFiles(files, "pdf")
-
-    print(files)
+    pdfFilesToRemove = FilesToRemove(files, "pdf")
+#    print(pdfFilesToRemove)
 
 
 if __name__ == "__main__":
